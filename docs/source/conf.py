752c4fa9ba7b8a66de477ff1f2e24428f5e057c0
wyl <1411452414@qq.com>
2026-09-11 16:09:45 +0800

    1

---
+++ docs/source/conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'D1_Max'
+copyright = '2026, Agibot'
+author = 'Agibot'
+release = 'V0.1.0'
+
+# -- General configuration ---------------------------------------------------
+extensions = ['myst_parser']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# sphinx‑intl i18n 设置
+language = "en"
+locale_dirs = ["../locale/"]
+gettext_compact = False
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_css_files = ["css/hide_top_select.css"]
+
+html_copy_source = False
+html_show_sourcelink = False
+html_show_sphinx = False
+html_secnumber_suffix = ' '
---
+++ docs/source/index.md
@@ -0,0 +1,11 @@
+AGIQUAD D1 Max Quadruped Robot SDK Development Guide
+=============================================
+
+```{toctree}
+:maxdepth: 2
+
+1.Product_Introduction
+2.SDK_Development_Guide
+3.SDK_Function_Introduction
+4.Sensor_Data_Retrieval
+5.Debugging_Instructions
