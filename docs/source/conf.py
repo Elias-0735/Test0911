c3c922c6f40cfcb38a0b706259a4953b094ea5bf
wyl <1411452414@qq.com>
2026-09-11 16:30:14 +0800

    1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ gettext_compact = False
 # -- Options for HTML output -------------------------------------------------
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-html_css_files = ["css/hide_top_select.css"]
+html_css_files = ["css/custom.css"]
 
 html_copy_source = False
 html_show_sourcelink = False
