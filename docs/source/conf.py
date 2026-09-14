1d87f06a1c9e8f34d7542f6d25b8801a7302ed2b
wyl <1411452414@qq.com>
2026-09-14 13:47:15 +0800

    1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,9 +25,30 @@ gettext_compact = False
 # -- Options for HTML output -------------------------------------------------
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ["_static"]
-html_js_files = [
-    "js/flyout-hide.js"
-]
+html_css_files = ["css/tippy.css", "css/custom.css", "css/all.min.css"]
+
+html_theme_options = {
+    # 'logo_only': True,
+    # 'display_version': True,
+    # 'anchor_links': False,
+    # 'touch_icon': '../icon.jpg',
+    # 'logo_icon': '&#xe869',
+    "logo_only": False,
+    "style_external_links": True,
+    "collapse_navigation": False,
+}
+
+mermaid_use_local = "js/mermaid@11.12.2/mermaid.esm.min.mjs"
+mermaid_elk_use_local = "js/mermaid-layout-elk@0.2.0/mermaid-layout-elk.esm.min.mjs"
+d3_use_local = "js/d3@7.9.0/d3.min.js"
+
+tippy_js = ("js/@popperjs/core@2", "js/tippy.js@6")
+tippy_props = {"theme": "material"}
+tippy_tip_selector = "figure, table, img, p, aside, div.admonition, div.literal-block-wrapper, ul"
+tippy_props = {"placement": "auto-start", "maxWidth": 500, "interactive": False,
+               "theme": "material", "duration": [300, 200], "delay": [200, 100]}
+
+html_favicon = '_static/favicon.ico'
 
 html_copy_source = False
 html_show_sourcelink = False
